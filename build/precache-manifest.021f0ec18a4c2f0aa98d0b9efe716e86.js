043a2ba93f9cc369c7027a98102d95a98cb16641
VanV-GBC <vanja.vego@georgebrown.ca>
2020-10-07 20:00:28 -0400

    added build and src folder, edited package.json

---
+++ build/precache-manifest.021f0ec18a4c2f0aa98d0b9efe716e86.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5aa36e9dde5b551a8f62",
+    "url": "/static/css/main.2384b27b.chunk.css"
+  },
+  {
+    "revision": "5aa36e9dde5b551a8f62",
+    "url": "/static/js/main.5aa36e9d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "206892ec38c99bdd1291",
+    "url": "/static/js/2.206892ec.chunk.js"
+  },
+  {
+    "revision": "ebdea62c48aa02dfd4805cf5be635491",
+    "url": "/index.html"
+  }
+];
