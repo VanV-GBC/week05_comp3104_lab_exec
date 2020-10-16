47b020570fe81bdf709a2cca21d56067d08631df
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-10-16 00:02:07 +0000

    Deploy VanV-GBC/week05_comp3104_lab_exec to github.com/VanV-GBC/week05_comp3104_lab_exec.git:gh-pages

---
+++ precache-manifest.9f88854c2ca0f2386fd241a3142928c4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bb296d4fd71744cfafcd",
+    "url": "/static/css/main.a3ab3ec5.chunk.css"
+  },
+  {
+    "revision": "bb296d4fd71744cfafcd",
+    "url": "/static/js/main.bb296d4f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "206892ec38c99bdd1291",
+    "url": "/static/js/2.206892ec.chunk.js"
+  },
+  {
+    "revision": "ab7b4c817b56d735fefe894cdcdce1ac",
+    "url": "/index.html"
+  }
+];
